70d3d83154e9ed828e1c45a60fd74cb718e0518d
Veronika Benkeser <veronika.benkeser@gmail.com>
2015-03-30 16:18:10 -0700

    Create ReversedArray.js

---
+++ Chapter-3/ReversedArray.js
@@ -0,0 +1,33 @@
+function range (start, end, step){
+    var arr=[];
+    
+    if (step == undefined)
+        step=1;
+        
+    if(end<start)
+        if (step>=0)
+            console.log("Cannot find the range.");
+        else 
+            for(i=start; i>=end; i+=step){
+                arr.push(i);
+        }
+    else if (end ===start)
+        console.log("Please enter 2 different numbers.");
+    else 
+    
+        if(step<=0)
+            console.log("Cannot find the range.");
+        else 
+            for(i=start; i<=end; i+=step){
+                arr.push(i);
+            }
+    return arr;
+};
+
+function sum(arr){
+    var result =0;
+    for(i=0; i<arr.length; i++){
+        result+=arr[i];
+    }
+    return result;
+};
